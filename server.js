c6c0cdd2ba591b21787ca0fd6c6e352ad13ad056
Riya-Parasar <rparasar@uncc.edu>
2023-09-16 02:53:59 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
-const exp = require("constants");
 const express = require('express');
+const fs = require('fs'); // Import the 'fs' module for file operations
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-myBudget: [
-    {
-        title: 'Eat out',
-        budget: 40
-    },
-    {
-        title: 'Rent',
-        budget:375
-    },
-    {
-        title: 'Grocery',
-        budget: 93
-    }
-    ]
-
-};
+// Read the budget data from the JSON file
+const budgetData = JSON.parse(fs.readFileSync('budget-data.json', 'utf8'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgetData);
 });
 
-app.listen(port, () =>
-{
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
