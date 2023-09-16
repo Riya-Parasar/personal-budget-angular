455a8e5f7c513eed80b40f47f08e3888e1498938
riya2 <rparasar@uncc.edu>
2023-09-16 01:55:51 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,37 @@
+const exp = require("constants");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+myBudget: [
+    {
+        title: 'Eat out',
+        budget: 40
+    },
+    {
+        title: 'Rent',
+        budget:375
+    },
+    {
+        title: 'Grocery',
+        budget: 93
+    }
+    ]
+
+};
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () =>
+{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
