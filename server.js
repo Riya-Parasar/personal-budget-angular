af98297c367053d48a67473b576248ebc5e9974d
Riya-Parasar <rparasar@uncc.edu>
2023-09-22 14:43:20 -0400

    First commit on Assisgnment 5

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs'); // Import the 'fs' module for file operations
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+//app.use('/', express.static('public'));
+app.use(cors());
 
 // Read the budget data from the JSON file
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 50
+        },
+        {
+            title: 'Rent',
+            budget:200
+        },
+        {
+            title: 'Grocery',
+            budget: 90
+        }
+        ]
+    
+    };
 const budgetData = JSON.parse(fs.readFileSync('budget-data.json', 'utf8'));
 
-app.get('/hello', (req, res) => {
+/*app.get('/hello', (req, res) => {
     res.send('Hello World!');
-});
+});*/
 
 app.get('/budget', (req, res) => {
     res.json(budgetData);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
